38260f218f301334e3fe2d4fed2e00bc60abef49
Huy Nguyen <quanghuynguyen1603@gmail.com>
2023-10-02 01:53:48 -0500

    delete import file for image link

--- pages/_import.js
+++
@@ -1,8 +0,0 @@
-import sign1 from '../public/images/sign_translator/sign1.png';
-import sign2 from '../public/images/sign_translator/sign2.png';
-import sign3 from '../public/images/sign_translator/sign3.png';
-
-// import image from '../public/images/hinh em2.jpg';
-const signLanguageList = [sign1, sign2, sign3];
-const imgList = { signLanguageList };
-export { imgList };
