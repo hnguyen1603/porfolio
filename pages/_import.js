7b5cfdb6b5795310cc745f1adf8a36fd0106698c
Huy Nguyen <quanghuynguyen1603@gmail.com>
2023-10-02 01:50:35 -0500

    nothing new

--- pages/_import.js
+++ pages/_import.js
@@ -5,4 +5,4 @@ import sign3 from '../public/images/sign_translator/sign3.png';
 // import image from '../public/images/hinh em2.jpg';
 const signLanguageList = [sign1, sign2, sign3];
 const imgList = { signLanguageList };
-export default imgList;
+export { imgList };
