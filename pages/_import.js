fb111d93a855f1f6b5544fd1b62bbb6f6f285637
Huy Nguyen <quanghuynguyen1603@gmail.com>
2023-10-02 01:44:33 -0500

    finish the porfolio with some minor details on project

---
+++ pages/_import.js
@@ -0,0 +1,8 @@
+import sign1 from '../public/images/sign_translator/sign1.png';
+import sign2 from '../public/images/sign_translator/sign2.png';
+import sign3 from '../public/images/sign_translator/sign3.png';
+
+// import image from '../public/images/hinh em2.jpg';
+const signLanguageList = [sign1, sign2, sign3];
+const imgList = { signLanguageList };
+export default imgList;
